3ca1e354a1e6fcf6eebfd7919bfe3264d04f4d16
JocelynTse <jocelyn.s.tse@gmail.com>
2018-05-22 18:28:13 -0500

    Update JS to add crystal clicks to Total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,74 @@
 
     let wins = 0;
     let losses = 0;
+    let total = 0;
+
+    let random = Math.floor(Math.random() * 120) + 19;
+    $("#random").text(random);
 
     let crystalRed = $("#Crystal-1");
     let crystalYellow = $("#Crystal-2");
     let crystalGreen = $("#Crystal-3");
     let crystalBlue = $("#Crystal-4");
 
+    let Red = Math.floor(Math.random() * 12) + 1;
+    let Yellow = Math.floor(Math.random() * 12) + 1;
+    let Green = Math.floor(Math.random() * 12) + 1;
+    let Blue = Math.floor(Math.random() * 12) + 1;
 
-    let random = Math.floor(Math.random() * 120) + 19;
 
-    $("#random").text(random);
-
-        
     $("#Crystal-1").on("click", function() {
-        let Red = Math.floor(Math.random() * 12) + 1;
-        console.log(Red);
-        $("#total").text(Red);
-    
+        total += Red;
+        console.log(total);
+        $("#total").text(total);
+        totalCheck();
+
     });
 
     $("#Crystal-2").on("click", function() {
-        let Yellow = Math.floor(Math.random() * 12) + 1;
-        console.log(Yellow);
-        $("#total").text(Yellow);
-    
+        total += Yellow;
+        console.log(total);
+        $("#total").text(total);
+        totalCheck();
+
     });
 
     $("#Crystal-3").on("click", function() {
-        let Green = Math.floor(Math.random() * 12) + 1;
-        console.log(Green);
-        $("#total").text(Green);
-    
+        total += Green;
+        console.log(total);
+        $("#total").text(total);
+        totalCheck();
+
     });
 
     $("#Crystal-4").on("click", function() {
-        let Blue = Math.floor(Math.random() * 12) + 1;
-        console.log(Blue);
-        $("#total").text(Blue);
-    
+        total += Blue;
+        console.log(total);
+        $("#total").text(total);
+        totalCheck();
+
     });
 
-
-    if (total === random)   {
+    function totalCheck()   {
+        if (total === random)   {
         wins++;
         $("#wins").text(wins);
-        random = 0;
-    } else if (total > random)  {
+        alert("You defeated the Crystal King!");
+
+        } else if (total > random)  {
         losses++;
         $("#losses").text(losses);
-        random = 0;
-    }
+        alert("You lost. Try again.");
+        reset();
+    };
+
+    function reset()    {
+        let random = Math.floor(Math.random() * 120) + 19;
+        let Red = Math.floor(Math.random() * 12) + 1;
+        let Yellow = Math.floor(Math.random() * 12) + 1;
+        let Green = Math.floor(Math.random() * 12) + 1;
+        let Blue = Math.floor(Math.random() * 12) + 1;
+    };
+
+    };
+
